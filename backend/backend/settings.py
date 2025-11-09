da7f4edd052a74718b7e7a3ebba3ce334c051a9c
Artyom <sindycate96@yandex.ru>
2025-11-09 17:43:21 +0300

    Some fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,10 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0'] # ['localhost', '123.123.123.123', '<your_domain>']
-
-
-# Application definition
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
